ed3d142bc91610ed237028bc117af938805057d4
ErwinDupuis <erwin.dupuis@gmail.com>
2017-04-01 18:06:21 +0200

    Create parametres.php

---
+++ parametres.php
@@ -0,0 +1,9 @@
+<?php
+$PARAM_hote = 'localhost'; // le chemin vers le serveur
+$PARAM_port='3306';
+$PARAM_nom_bd='gestionfromage'; // le nom de votre base de données
+$PARAM_utilisateur='adminFromage'; // nom d'utilisateur pour se connecter
+$PARAM_mot_passe='ef749ff9a048bad0dd80807fc49e1c0d'; // mot de passe de l'utilisateur pour se connecter
+define('PARAM_pathPhoto','/service_web_gestionfromage/Images/');
+define('PARAM_extensionPhoto', '.jpg');
+?>
